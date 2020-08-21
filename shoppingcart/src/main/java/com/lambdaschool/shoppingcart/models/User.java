88e291d08997967fd2fcd3982504dc7e1c9a2e4c
JDMTias <matiasiturbide@gmail.com>
2020-08-20 22:45:09 -0600

    almost done, error about a bean

--- shoppingcart/src/main/java/com/lambdaschool/shoppingcart/models/User.java
+++ shoppingcart/src/main/java/com/lambdaschool/shoppingcart/models/User.java
@@ -1,7 +1,10 @@
 package com.lambdaschool.shoppingcart.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,7 +14,9 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
@@ -21,61 +26,90 @@ public class User
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long userid;
-
     @Column(nullable = false,
             unique = true)
     private String username;
-
+    private String password;
     private String comments;
-
     @OneToMany(mappedBy = "user",
             cascade = CascadeType.ALL)
     @JsonIgnoreProperties(value = "user",
             allowSetters = true)
     private List<Cart> carts = new ArrayList<>();
-
+    @OneToMany(mappedBy = "user",
+            cascade = CascadeType.ALL)
+    @JsonIgnoreProperties(value = "users",
+            allowSetters = true)
+    private Set<UserRoles> roles = new HashSet<>();
     public User()
     {
-
     }
-
+    public User(long userid, String username, String password, String comments, List<Cart> carts, Set<UserRoles> roles) {
+        this.userid = userid;
+        this.username = username;
+        this.password = password;
+        this.comments = comments;
+        this.carts = carts;
+        this.roles = roles;
+    }
+    public Set<UserRoles> getRoles() {
+        return roles;
+    }
+    public void setRoles(Set<UserRoles> roles) {
+        this.roles = roles;
+    }
     public long getUserid()
     {
         return userid;
     }
-
     public void setUserid(long userid)
     {
         this.userid = userid;
     }
-
     public String getUsername()
     {
         return username;
     }
-
     public void setUsername(String username)
     {
         this.username = username;
     }
-
     public String getComments()
     {
         return comments;
     }
-
     public void setComments(String comments)
     {
         this.comments = comments;
     }
-
     public List<Cart> getCarts()
     {
         return carts;
     }
-
     public void setCarts(List<Cart> carts)
     {
         this.carts = carts;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password)
+    {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        this.password = passwordEncoder.encode(password);
+    }
+    public void setPasswordNoEncrypt(String password)
+    {
+        this.password = password;
+    }
+    @JsonIgnore
+    @Transactional
+    public List<SimpleGrantedAuthority> getAuthority() {
+        List<SimpleGrantedAuthority> rtnList = new ArrayList<>();
+        for(UserRoles r : this.roles ) {
+            String string  = "ROLE" + r.getRole().getName().toUpperCase();
+            rtnList.add(new SimpleGrantedAuthority(string));
+        }
+        return rtnList;
     }
 }
